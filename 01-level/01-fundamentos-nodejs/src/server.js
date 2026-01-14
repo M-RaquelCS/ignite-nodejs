00179c5eb4a21bfe37f15b56844c30b6672f3dae
Raquel Santos <m-raquel@outlook.com.br>
2026-01-13 22:59:38 -0300

    feat: http status code example

--- 01-level/01-fundamentos-nodejs/src/server.js
+++ 01-level/01-fundamentos-nodejs/src/server.js
@@ -7,6 +7,8 @@ import http from "node:http";
 
 // Cabeçalhos (Requisição/resposta) => Metadados
 
+// HTTP Status Code
+
 const users = [];
 
 const server = http.createServer((req, res) => {
@@ -25,10 +27,10 @@ const server = http.createServer((req, res) => {
 			email: "johndoe@example.com",
 		});
 
-		return res.end("Criação de usuário");
+		return res.writeHead(201).end();
 	}
 
-	return res.end("Hello World");
+	return res.writeHead(404).end();
 });
 
 server.listen(3333);
