6b664c824af4b83883c5710bad3f6ba2c5abbf5a
raquel.santos <raquel.santos@grupomoura.com>
2026-01-14 14:35:38 -0300

    feat: add req body

--- 01-level/01-fundamentos-nodejs/src/server.js
+++ 01-level/01-fundamentos-nodejs/src/server.js
@@ -14,6 +14,18 @@ const users = [];
 const server = http.createServer((req, res) => {
 	const { method, url } = req;
 
+	const buffers = []
+
+	for await (const chunk of req) {
+		buffers.push(chunk)
+	}
+
+	try {
+		req.body = JSON.parse(Buffer.concat(buffers).toString())
+	} catch {
+		req.body = null
+	}
+
 	if (method === "GET" && url === "/users") {
 		return res
 			.setHeader("Content-type", "application/json")
@@ -21,10 +33,11 @@ const server = http.createServer((req, res) => {
 	}
 
 	if (method === "POST" && url === "/users") {
+		const { name, email } = req.body
 		users.push({
 			id: 1,
-			name: "John Doe",
-			email: "johndoe@example.com",
+			name,
+			email
 		});
 
 		return res.writeHead(201).end();
