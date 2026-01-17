8b3f4712ec178532a75ed77b4c2e8f071642d1a3
Raquel Santos <m-raquel@outlook.com.br>
2026-01-17 15:32:54 -0300

    feat: buffer example

--- 01-level/01-fundamentos-nodejs/streams/stream-http-server.js
+++ 01-level/01-fundamentos-nodejs/streams/stream-http-server.js
@@ -1,33 +1,31 @@
+import http from "node:http";
 import { Transform } from "node:stream";
-import http from 'node:http'
 
 class InverseNumberStream extends Transform {
-    _transform(chunk, encoding, callback) {
-        const transformed = Number(chunk.toString()) * -1;
+	_transform(chunk, encoding, callback) {
+		const transformed = Number(chunk.toString()) * -1;
 
-        console.log(transformed);
+		console.log(transformed);
 
-        callback(null, Buffer.from(String(transformed)));
-    }
+		callback(null, Buffer.from(String(transformed)));
+	}
 }
-
 
 // req => ReadableStream
 // res => WritableStream
 
-
 const server = http.createServer(async (req, res) => {
-    const buffers = []
+	const buffers = [];
 
-    for await (const chunk of req) {
-        buffers.push(chunk)
-    }
+	for await (const chunk of req) {
+		buffers.push(chunk);
+	}
 
-    const fullStreamContent = Buffer.concat(buffers).toString()
+	const fullStreamContent = Buffer.concat(buffers).toString();
 
-    console.log(fullStreamContent)
+	console.log(fullStreamContent);
 
-    return res.end(fullStreamContent)
-})
+	return res.end(fullStreamContent);
+});
 
-server.listen(3334)
+server.listen(3334);
