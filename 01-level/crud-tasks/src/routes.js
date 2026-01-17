bd702118df52aaf6013670c1268d0ab581ee4b1c
Raquel Santos <m-raquel@outlook.com.br>
2026-01-17 19:48:59 -0300

    feat: create and get (search) routes

---
+++ 01-level/crud-tasks/src/routes.js
@@ -0,0 +1,64 @@
+import { randomUUID } from "node:crypto";
+import { Database } from "./database/db.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
+
+const db = new Database();
+
+export const routes = [
+	{
+		method: "POST",
+		path: buildRoutePath("/tasks"),
+		handler: (req, res) => {
+			const { title, description } = req.body;
+
+			const hasTitle = typeof title === "string" && title.trim() !== "";
+			const hasDescription =
+				typeof description === "string" && description.trim() !== "";
+
+			if (!hasTitle || !hasDescription) {
+				return res
+					.writeHead(400, { "Content-Type": "application/json" })
+					.end(JSON.stringify({ error: "Informe title e/ou description." }));
+			}
+			const now = new Date();
+
+			const task = {
+				id: randomUUID(),
+				title,
+				description,
+				created_at: now,
+				updated_at: now,
+				completed_at: null,
+			};
+
+			db.insert("tasks", task);
+			return res.writeHead(201).end();
+		},
+	},
+	{
+		method: "GET",
+		path: buildRoutePath("/tasks"),
+		handler: (req, res) => {
+			const { search } = req.query;
+			const tasks = db.select(
+				"tasks",
+				search
+					? {
+							title: search,
+							description: search,
+						}
+					: null,
+			);
+
+			if (!tasks.length) {
+				return res
+					.writeHead(400, { "Content-Type": "application/json" })
+					.end(JSON.stringify({ error: "Sem nenhuma atividade cadastrada." }));
+			}
+
+			return res
+				.writeHead(200, { "Content-Type": "application/json" })
+				.end(JSON.stringify(tasks));
+		},
+	},
+];
