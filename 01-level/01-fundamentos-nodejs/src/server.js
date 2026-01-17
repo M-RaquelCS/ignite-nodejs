0baeb90c72ac4acd93a190561b83f30f2007dc0c
Raquel Santos <m-raquel@outlook.com.br>
2026-01-17 16:07:20 -0300

    feat: database

--- 01-level/01-fundamentos-nodejs/src/server.js
+++ 01-level/01-fundamentos-nodejs/src/server.js
@@ -1,4 +1,5 @@
 import http from "node:http";
+import { Database } from "./database.js";
 import { json } from "./middlewares/json.js";
 
 // GET /users => Buscando usuários no banc-end
@@ -10,7 +11,7 @@ import { json } from "./middlewares/json.js";
 
 // HTTP Status Code
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
 	const { method, url } = req;
@@ -18,16 +19,20 @@ const server = http.createServer(async (req, res) => {
 	await json(req, res);
 
 	if (method === "GET" && url === "/users") {
+		const users = database.select("users");
+
 		return res.end(JSON.stringify(users));
 	}
 
 	if (method === "POST" && url === "/users") {
 		const { name, email } = req.body;
-		users.push({
+		const user = {
 			id: 1,
 			name,
 			email,
-		});
+		};
+
+		database.insert("users", user);
 
 		return res.writeHead(201).end();
 	}
