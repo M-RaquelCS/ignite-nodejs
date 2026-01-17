7e080312eaf048501748224764e90677ee28b087
Raquel Santos <m-raquel@outlook.com.br>
2026-01-17 17:07:31 -0300

    feat: remove user

--- 01-level/01-fundamentos-nodejs/src/server.js
+++ 01-level/01-fundamentos-nodejs/src/server.js
@@ -11,16 +11,33 @@ import { routes } from "./routes.js";
 
 // HTTP Status Code
 
+// Query Parameters: URL Stateful => Filtros, paginação, não-obrigatórios
+// Route Parameters: Identificação de recurso
+// Request Body: Envio de informações de um formulário (HTTPs)
+
+//http://localhost:3333/users?userId=1&name=Diego
+
+// GET http://localhost:3333/users/1
+// DELETE http: //localhost:3333/users/1
+
+// POST http://localhost:3333/users
+
+// Edição e remoção
+
 const server = http.createServer(async (req, res) => {
 	const { method, url } = req;
 
 	await json(req, res);
 
 	const route = routes.find((route) => {
-		return route.method === method && route.path === url;
+		return route.method === method && route.path.test(url);
 	});
 
 	if (route) {
+		const routeParams = req.url.match(route.path);
+
+		req.params = { ...routeParams.groups };
+
 		return route.handler(req, res);
 	}
 
